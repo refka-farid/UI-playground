0d257aa8f26a6c3a673b328c89204e00fac5bd00
refka-farid <refka.farid@gmail.com>
2020-01-28 12:37:23 +0100

    wip

---
+++ playground/src/main/java/com/bravedroid/playground/HomeActivity.kt
@@ -0,0 +1,42 @@
+package com.bravedroid.playground
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.layout_activityhome.*
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_activityhome)
+
+        val callback: (View) -> Unit = {view ->
+            when (view.id) {
+                R.id.user_input_cardview -> {
+                }
+                R.id.output_content_cardview -> {
+                }
+                R.id.widgets_cardview -> {
+                }
+                R.id.notifications_cardview -> {
+                    view.findNavController().navigate(
+                            HomeActivityDirections.actionHomeActivityToNotificationsActivity2()
+                    )
+                }
+                R.id.tool_cardview -> {
+                }
+                R.id.samples_cardview -> {
+                }
+            }
+        }
+
+        user_input_cardview.setOnClickListener(callback)
+        output_content_cardview.setOnClickListener(callback)
+        widgets_cardview.setOnClickListener(callback)
+        notifications_cardview.setOnClickListener(callback)
+        tool_cardview.setOnClickListener(callback)
+        samples_cardview.setOnClickListener(callback)
+
+    }
+}
--- playground/src/main/java/com/bravedroid/playground/NotificationsActivity.kt
+++ playground/src/main/java/com/bravedroid/playground/NotificationsActivity.kt
@@ -1,17 +1,39 @@
 package com.bravedroid.playground
 
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.bravedroid.ui.notifications.toast.ToastHelper
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_notification.*
 
 class NotificationsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_toast)
-        findViewById<TextView>(R.id.textView_main_messageToaster).setOnClickListener {
-            ToastHelper.show(this,"this is a test !!!!!")
+        setContentView(R.layout.activity_notification)
+
+        val callback: (View) -> Unit = { view ->
+            when (view.id) {
+                R.id.toast_types_btn -> {
+                    view.findNavController().navigate(
+                            NotificationsActivityDirections.actionNotificationsActivity2ToToastActivity()
+                    )
+                }
+                R.id.alert_dialog_types_btn -> {
+                    view.findNavController().navigate(
+                            NotificationsActivityDirections.actionNotificationsActivity2ToAlertDialogActivity()
+                    )
+                }
+                R.id.snackbar_types_btn -> {
+                    view.findNavController().navigate(
+                            NotificationsActivityDirections.actionNotificationsActivity2ToSnackbarActivity()
+                    )
+                }
+            }
         }
+
+        toast_types_btn.setOnClickListener { callback }
+        alert_dialog_types_btn.setOnClickListener(callback)
+        snackbar_types_btn.setOnClickListener(callback)
     }
 }
